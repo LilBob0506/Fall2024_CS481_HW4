678ce7e71c60ab622bed0de08fcedd5faf9e3484
LilBob0506 <131300719+LilBob0506@users.noreply.github.com>
2024-11-13 18:18:40 -0600

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -7,5 +7,18 @@ module load openmpi/4.1.4-gcc11
 mpicc -g -Wall -O -o life_mpi life_mpi.c
 
 mpirun -np 1 ./life_mpi 5000 5000 /scratch/ualclsd0193/
-mpirun -np 1 ./life_mpi 5000 5000 /scratch/ualclsd0193/
-mpirun -np 1 ./life_mpi 5000 5000 /scratch/ualclsd0193/
+mpirun -np 2 ./life_mpi 5000 5000 /scratch/ualclsd0193/
+mpirun -np 4 ./life_mpi 5000 5000 /scratch/ualclsd0193/
+mpirun -np 8 ./life_mpi 5000 5000 /scratch/ualclsd0193/
+mpirun -np 10 ./life_mpi 5000 5000 /scratch/ualclsd0193/
+mpirun -np 16 ./life_mpi 5000 5000 /scratch/ualclsd0193/
+mpirun -np 20 ./life_mpi 5000 5000 /scratch/ualclsd0193/
+
+cd /scratch/ualclsd0193
+
+diff output.5000.5000.1.txt output.5000.5000.2.txt
+diff output.5000.5000.2.txt output.5000.5000.4.txt
+diff output.5000.5000.2.txt output.5000.5000.8.txt
+diff output.5000.5000.2.txt output.5000.5000.10.txt
+diff output.5000.5000.2.txt output.5000.5000.16.txt
+diff output.5000.5000.2.txt output.5000.5000.20.txt
